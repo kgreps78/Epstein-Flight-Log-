e3a1f561727b4c9e9390a97ee459ca31123d3d98
kgreps78 <jenklerohwhenthesaints@gmail.com>
2025-11-28 22:17:49 -0700

    scrhstest - Copy.py

--- scrhstest - Copy.py
+++ scrhstest - Copy.py
@@ -4,10 +4,11 @@ if passcheck == "jevacationsigma":
     print("password is correct\n")
     print("Donald Trump\nBill Clinton\nJeff Bezos\nElon Musk\n")
     destlist = input("Destroy list?")
-    if destlist == "Yes":
+    if destlist == "Yes" or destlist == "yes":
         print("List has been Deleted")
     else:print("-271 Goy Points, Private Military is getting sent to your house") 
 else:print("ACCESS DENIED\nMossad Agents are en route to your house right now\nDownloading file: MANIFESTO.TXT to e:/ohiosigmabluds\nDOWNLOAD COMPLETE")
 
         
+
 
