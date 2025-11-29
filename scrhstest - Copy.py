c8eaea40f8910b689cb9e09a9e1882f59b83e6bf
kgreps78 <jenklerohwhenthesaints@gmail.com>
2025-11-28 23:36:53 -0700

    Update scrhstest - Copy.py

--- scrhstest - Copy.py
+++ scrhstest - Copy.py
@@ -1,14 +1,19 @@
 print("THE EPSTEIN LOG BOOK\n")
+usercheck = input("USERNAME:")
 passcheck = input("PASSWORD:")
 if passcheck == "jevacationsigma":
     print("password is correct\n")
-    print("Donald Trump\nBill Clinton\nJeff Bezos\nElon Musk\n")
-    destlist = input("Destroy list?")
+    print("Donald Trump\nBill Clinton\nJeff Bezos\nElon Musk\nBill Gates\nPrince Andrew\nDiddy\n")
+    destlist = input("Destroy list?\n")
     if destlist == "Yes" or destlist == "yes":
-        print("List has been Deleted")
+        print("List has been Wiped\n")
+        fakepalestine = input("send another $2 Billion to Israel?\n")
+        if fakepalestine == "Yes" or fakepalestine == "yes":
+            print("+6,000,000 Goy Points, Good Job Senator\n")
+            areyousigma = input("Are you Indian?\n")
+            if areyousigma == "Yes" or areyousigma == "yes":
+                print("Board Epsteins jet at 07:00 AM tomorrow morning, your going to the Island for free blud")
+            else:print("Modi gonna diddle you lil bro")
+        else:print("Your gonna get kirked lil bro")
     else:print("-271 Goy Points, Private Military is getting sent to your house") 
 else:print("ACCESS DENIED\nMossad Agents are en route to your house right now\nDownloading file: MANIFESTO.TXT to e:/ohiosigmabluds\nDOWNLOAD COMPLETE")
-
-        
-
-
